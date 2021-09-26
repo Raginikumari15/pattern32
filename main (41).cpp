1bde29531bf67392e9b48542d2cfe3fa481867f8
Ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-09-27 02:49:45 +0530

    Add files via upload

---
+++ main (41).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {                               /* 55555
+                                              45555
+                                              34555
+                                              23455
+                                              12345*/
+int i = 5, j, k;
+while(i >= 1) {
+k = i;
+j = 1;
+while(j <= 5) {
+if(k <= 5) {
+cout << k;
+}
+else {
+cout << 5;
+}
+k = k + 1;
+j = j + 1;
+}
+cout << endl;
+i = i - 1;
+}
+}
